f3bca9f8dd4d8ed52c763809f44a7601d14f1a03
Christian Habib <chrha376@student.liu.se>
2017-11-15 16:24:55 +0100

    Cleaned up com module code

--- src/com_module.c
+++ src/com_module.c
@@ -1,111 +1,47 @@
-/*
-*
- * UART_transm.c
- *
- * Created: 11/10/2017 9:07:26 AM
- *  Author: nisan016
- */
+
  
 #include <avr/io.h>
 #include <util/delay.h>
 #include <stdio.h>
 #include <avr/interrupt.h>
+#include "USART_usb.h"
+#include "UART_sensor.h"
+
 #define BAUD 115200
 #define F_CPU 14745600 // set clock frekvens after pdf
 #define UDDRV ((F_CPU)/(16*BAUD)-1)
 
-void UART_init (void);
-void UART_transmitByte (unsigned char data);
-unsigned char UART_recieveByte(void);
-unsigned int c= 0;
 volatile unsigned char dtemp;
-volatile unsigned char data_received;
+volatile unsigned char data_received_usb;
+volatile unsigned char data_received_sensor;
 
 int main(void)
 {
 	
 	DDRB = 0xFF;
 	
-	
+	sei();
 	UART_init();
+	UART_usb_init();
     while(1)
     {
-		
-        //TODO:: Please write your application code
-		cli();
-		dtemp = data_received;
-		sei();
-		if (dtemp == 'a'){
-		PORTB |= (1<<PINB2);
-		
-		}
-	
-		else if(dtemp == 'd'){
-		PORTB |= (1<<PINB3);
-		
-		
-		
-		}
-		else if(dtemp == 'w') {
-		PORTB |= (1<<PINB4);
-		
-		
-		
-		}
-		else if(dtemp == 's'){
-			PORTB |= (1<<PINB5);
-			
-		
-
-		}else{
-			PORTB &= (0<<PINB2);
-			PORTB &= (0<<PINB3);
-			PORTB &= (0<<PINB4);
-			PORTB &= (0<<PINB5);
-				
-		}
-		
-		
+        
     
 		}
 }
 ISR(USART0_RX_vect){ 
-	data_received= UDR0;
+
+	data_received_usb= UDR0;
+	//PORTB &= (0<<PINB2);
+	//PORTB &= (0<<PINB3);
+	//PORTB &= (0<<PINB4);
+	//	PORTB &= (0<<PINB5);
 	
 	
-	
-	UART_transmitByte(data_received);
+	UART_usb_transmitByte(data_received);
 
 	
 }
-void UART_init (void)
-{
-	// Set all needed register
-	//PRR0 &= ~(1<<PRUSART0);
-	UBRR0H = (UDDRV>>8);
-	UBRR0L = UDDRV;
-	#if USE_2X
-	UCSR0A |= (1<<U2X0);
-	#else
-	UCSR0A &= ~(1<<U2X0);
-	#endif
-	UCSR0B |= ((1 << TXEN0)|(1 << RXEN0)|(1<<RXCIE0)); // set transmitter enable and receiver enable to 1 to indicate UART is ready to RX and TX
-	UCSR0C |= (1 << UCSZ00)|(1 << UCSZ10); // select UCSR0C and set data bits to 8 did not find URSEL
-	
-	
-	
-}	
-void UART_transmitByte (unsigned char data)
-{
-	while(!(UCSR0A & (1 << UDRE0))){} // UDRE is 1 then there is data in buffet
-	UDR0 = data; // send data to data register
-	
-}
-unsigned char UART_recieveByte(void)
-{
-	while(!((UCSR0A) & (1 << RXC0))); // check if we receive something
-	
-	return UDR0;
-	
-	
+ISR(USART1_RX_vect){
+	data_received_sensor = UDR1;
 }
