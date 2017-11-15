94bd148e2b34101c8ea51fee00e854fe630ad4e4
Christian Habib <chrha376@student.liu.se>
2017-11-15 18:59:13 +0100

    forgot interrupt enable

--- src/UART_sensor.h
+++ src/UART_sensor.h
@@ -1,3 +1,4 @@
+
 #define F_CPU 14745600 // set clock frekvens after pdf
 #define BAUD 115200
 #define UDDRV ((F_CPU)/(16*BAUD)-1)
@@ -15,19 +16,19 @@ void UART_init (void)
 	#else
 	UCSR1A &= ~(1<<U2X1);
 	#endif
-	UCSR1B |= ((1 << TXEN1)|(1 << RXEN1));//|(0<<UCSZ02)|(0<<RXCIE0)); // set transmitter enable and receiver enable to 1 to indicate UART is ready to RX and TX
+	UCSR1B |= ((1 << TXEN1)|(1 << RXEN1)|(1<<RXCIE1)); // set transmitter enable and receiver enable to 1 to indicate UART is ready to RX and TX
 	UCSR0C |= (1 << UCSZ10)|(1 << UCSZ11); // select UCSR0C and set data bits to 8 did not find URSEL
 	
 	
 	
 }	
-void UART_transmitByte (unsigned char data)
+void UART_transmitByte ( unsigned char data)
 {
 	while(!(UCSR1A & (1 << UDRE1))){} // UDRE is 1 then there is data in buffet
 	UDR1 = data; // send data to data register
 	
 }
-unsigned char UART_recieveByte(void)
+ unsigned char UART_recieveByte(void)
 {
 	while(!((UCSR1A) & (1 << RXC1))); // check if we receive something
 	
