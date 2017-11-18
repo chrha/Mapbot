d2a06f1aef5ed4d35f2f076f1a3701cf983fc413
Ilian Ayoub <iliay038@student.liu.se>
2017-11-18 17:21:40 +0100

    Update com_module.c: added PID regulator

--- src/com_module.c
+++ src/com_module.c
@@ -1,10 +1,8 @@
-
- 
 #include <avr/io.h>
 #include <util/delay.h>
 #include <stdio.h>
 #include <avr/interrupt.h>
-#include "USART_usb.h"
+#include "UART_USB.h"
 #include "UART_sensor.h"
 
 #define BAUD 115200
@@ -15,6 +13,20 @@ volatile unsigned char dtemp;
 volatile unsigned char data_received_usb;
 volatile unsigned char data_received_sensor;
 
+float KP=0;
+float KI=0;
+float KD=0;
+float latest_error=0;
+float I=0;
+float P;
+float desired_value=0.01;
+float actual_value;
+float D;
+float PID_value;
+float error;
+
+void PID(void);
+
 int main(void)
 {
 	
@@ -23,14 +35,14 @@ int main(void)
 	sei();
 	UART_init();
 	UART_usb_init();
-    while(1)
-    {
-        
-    
-		}
+	
+	while(1)
+	{
+		
+		
+	}
 }
-ISR(USART0_RX_vect){ 
-
+ISR(USART0_RX_vect){
 	data_received_usb= UDR0;
 	//PORTB &= (0<<PINB2);
 	//PORTB &= (0<<PINB3);
@@ -38,10 +50,46 @@ ISR(USART0_RX_vect){
 	//	PORTB &= (0<<PINB5);
 	
 	
-	UART_usb_transmitByte(data_received);
-
+	UART_usb_transmitByte(data_received_usb);
 	
 }
 ISR(USART1_RX_vect){
 	data_received_sensor = UDR1;
+}
+
+void PID(){
+	
+	error= desired_value - actual_value;
+	
+	P= error;
+	I += error;
+	D = (error - latest_error);
+	
+	PID_value= KP*P + KD* D + KI*I;
+	
+	UART_transmitByte(PID_value);      
+	
+	if( error < 0){              // rotate left
+		PORTB |= (1 << PINB2);
+		PORTB &= (0<<PINB3);
+		PORTB &= (0<<PINB4);
+		PORTB &= (0<<PINB5);
+	}
+	else if(error > desired_value){ // rotate right
+		PORTB |= (1 << PINB3);
+		PORTB &= (0<<PINB2);
+		PORTB &= (0<<PINB4);
+		PORTB &= (0<<PINB5);
+		
+	}
+	else{
+		PORTB |= (1 << PINB4);   // forward
+		PORTB &= (0<<PINB3);
+		PORTB &= (0<<PINB2);
+		PORTB &= (0<<PINB5);
+	}
+	
+	latest_error=error;
+	
+	
 }
