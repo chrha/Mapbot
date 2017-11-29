466f70fe6dce13fb76262bcea6a5d3adf1deac55
Ilian Ayoub <iliay038@student.liu.se>
2017-11-29 10:10:09 +0100

    Update PID(), lade till parallell koppling till sensormodul.

--- src/com_module.c
+++ src/com_module.c
@@ -2,63 +2,83 @@
 #include <util/delay.h>
 #include <stdio.h>
 #include <avr/interrupt.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
 #include "UART_USB.h"
 #include "UART_sensor.h"
 
-#define BAUD 115200
-#define F_CPU 14745600 // set clock frekvens after pdf
-#define UDDRV ((F_CPU)/(16*BAUD)-1)
-
-volatile unsigned char dtemp;
 volatile unsigned char data_received_usb;
 volatile unsigned char data_received_sensor;
+signed char KP=12;
+signed char KI=0;
+signed char KD=7; // value 7
+volatile signed char sensor1=0;
+volatile signed char sensor2=0;
 
-float KP=0;
-float KI=0;
-float KD=0;
-float latest_error=0;
-float I=0;
-float P;
-float desired_value=0.01;
-float actual_value;
-float D;
-float PID_value;
-float error;
+volatile int i=0;
+signed char latest_error=0;
+signed char I=0;
+signed char P;
+signed char desired_value=0;
+signed char actual_value;
+signed char D;
+signed char PID_value;
+signed char error;
+volatile uint8_t temp0;
+volatile uint8_t temp1;
+
 
 void PID(void);
+
 
 int main(void)
 {
 	
 	DDRB = 0xFF;
+	DDRA =0x00;
 	
-	sei();
 	UART_init();
 	UART_usb_init();
-	
+	sei();
 	while(1)
-	{
+	{	
 		
+	
+		PID();
+	
 		
 	}
+			
+		
 }
+
 ISR(USART0_RX_vect){
 	data_received_usb= UDR0;
-	//PORTB &= (0<<PINB2);
-	//PORTB &= (0<<PINB3);
-	//PORTB &= (0<<PINB4);
-	//	PORTB &= (0<<PINB5);
+
 	
-	
-	UART_usb_transmitByte(data_received_usb);
 	
 }
 ISR(USART1_RX_vect){
+
 	data_received_sensor = UDR1;
+		
+		temp0 = PINA & 0b00000001;
+		temp1 = PINA & 0b00000010;
+		if(temp0 == 1){
+			sensor1= data_received_sensor;
+		}
+		else if(temp1 == 2){
+			sensor2=data_received_sensor;
+		} 
+	
+	
 }
+
 
 void PID(){
 	
+	actual_value= sensor1- sensor2;
 	error= desired_value - actual_value;
 	
 	P= error;
@@ -68,29 +88,14 @@ void PID(){
 		I=100;
 	}
 	PID_value= KP*P + KD* D + KI*I;
+
 	
-	UART_transmitByte(PID_value);      
-	
-	if( PID_value < 0){              // rotate right
-		PORTB |= (1 << PINB3);
-		PORTB &= (0<<PINB3);
-		PORTB &= (0<<PINB4);
-		PORTB &= (0<<PINB5);
-	}
-	else if(PID_value > 0){ // rotate left
-		PORTB |= (1 << PINB2);
-		PORTB &= (0<<PINB2);
-		PORTB &= (0<<PINB4);
-		PORTB &= (0<<PINB5);
-		
-	}
-	else{
-		PORTB |= (1 << PINB4);   // forward
-		PORTB &= (0<<PINB3);
-		PORTB &= (0<<PINB2);
-		PORTB &= (0<<PINB5);
-	}
-	latest_error=error;
+
+	UART_transmitByte(PID_value);
 	
 	
-}
+	latest_error=error;      
+	
+	
+	
+} 
