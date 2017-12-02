7040923251fd7b1e5d058b0248a99b4e1f1d798c
Ilian Ayoub <iliay038@student.liu.se>
2017-12-02 12:10:27 +0100

    Update cont_module.c, with new pin check

--- src/cont_module.c
+++ src/cont_module.c
@@ -9,7 +9,8 @@ void pwm_init();
 void pwm_drive();
 void rotate_left();
 void rotate_right();
-void dir_foreward(signed char pid);
+void dir_pid_foreward(signed char pid);
+void dir_foreward(void);
 void dir_backward();
 void where_to_go(void);
 signed char pid=0;
@@ -17,6 +18,7 @@ uint8_t temp0;
 uint8_t temp1;
 uint8_t temp2;
 uint8_t temp3;
+uint8_t temp4;
 const unsigned char const_speed = 150;
 int main(void)
 {
@@ -28,7 +30,7 @@ int main(void)
     while(1)
     {
 	
-		//dir_foreward(pid);
+		
 		where_to_go();
 						
     }
@@ -52,7 +54,7 @@ void pwm_drive()
 	OCR3A = const_speed; // right
 
 }
-void dir_foreward(signed char pid)
+void dir_pid_foreward(signed char pid)
 {
 	
 	OCR3A = const_speed + pid;
@@ -62,19 +64,27 @@ void dir_foreward(signed char pid)
 	
 	
 }
+
+void dir_foreward(){
+	
+	PORTD |= (1 << PORTD1); // Pin PD0 goes high (forward_right)
+	PORTD |= (1 << PORTD0); // Pin PD1 goes high (forward_left)
+	pwm_drive();
+}
+
 void dir_backward()
 {
 	PORTD &= ~(1 << PORTD1); // Pin PD0 goes low (forward_right)
 	PORTD &= ~(1 << PORTD0); // Pin PD1 goes low (forward_left)
 	pwm_drive();
 }
-void rotate_left()
+void rotate_right()
 {
 	PORTD &= ~(1 << PORTD1); // Pin PD0 goes low (forward_right)
 	PORTD |= (1 << PORTD0); // Pin PD1 goes low (forward_left)
 	pwm_drive();
 }
-void rotate_right()
+void rotate_left()
 {
 	PORTD &= ~(1 << PORTD0); // Pin PD0 goes low (forward_right)
 	PORTD |= (1 << PORTD1); // Pin PD1 goes low (forward_left)
@@ -84,29 +94,31 @@ void rotate_right()
 void where_to_go(void)
 {
 	
-	temp0= PINA & 0b00000001;
-	temp1= PINA & 0b00000010;
-	temp2= PINA & 0b00000100;
-	temp3= PINA & 0b00001000;
+	temp0= PINA & 0b00000111;
+
 	
 	if(temp0 == 1){
 		
-		rotate_left();
-		
-	}
-	else if(temp1 == 2){
 		rotate_right();
 		
 	}
-	else if(temp2 == 4){
-		dir_foreward(pid);
+	else if(temp0 == 2){
+		rotate_left();
 		
 	}
-	else if(temp3 == 8){
+	else if(temp0 == 3){
+		dir_pid_foreward(pid);
+			
+	}
+	else if(temp0 == 4){
 		dir_backward();
 			
-	}else{
+	}else if(temp0 == 5){
+		dir_foreward();
+	
+	}
+	else{
 		OCR3B = 0; 
 		OCR3A = 0;
 	}
-} 
+}
