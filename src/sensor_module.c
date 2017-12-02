dd525ff9e170c9df5a6126e82f4e925480b1c863
Ilian Ayoub <iliay038@student.liu.se>
2017-12-02 12:07:33 +0100

    Update sensor_module.c, with distance sesors

--- src/sensor_module.c
+++ src/sensor_module.c
@@ -3,24 +3,46 @@
 #include <math.h>
 #include <util/delay.h>
 #include "UART_sensor.h"
-
+#include "I2C.h"
 #include <stdio.h>
 #include <string.h>
 void adc_init();
 uint8_t adc_read(uint8_t ch);
 void SendString(char mydata[20]);
-void terminal_view (int8_t adc_channel1, uint8_t adc_channel2);
+void terminal_view (uint8_t adc_channel1, uint8_t adc_channel2);
 uint8_t temp0, temp1;
 void what_to_send(void);
 uint16_t s1=0;
 uint16_t s2=0;
 signed char actual_value=0;
+void sensor_right_front(void);
+void sensor_right_back(void);
+void sensor_front(void);
+void sensor_left(void);
+void sensor_gyro(void);
+void sensor_distance(void);
+
+uint8_t lenght_right();
+uint8_t lenght_left();
+uint8_t black_4=0;
+uint8_t white_4=0;
+uint8_t black_5=0;
+uint8_t white_5=0;
+uint8_t PB_4=0;
+uint8_t PB_5=0;
+uint8_t latest_color1=0;
+uint8_t latest_color2=0;
+uint8_t average_distance=0;
+uint8_t left_distance=0;
+uint8_t right_distance=0;
+
 int main(void)
 {
-	DDRB = 0xFF;
+	DDRB = 0x0F;
 	adc_init();
 	UART_init();
-	
+	I2C_init();
+	I2C_power();
 	
 	while(1)
 	{
@@ -28,19 +50,14 @@ int main(void)
 		//s2= (uint16_t)(floor(10*(27.6649/pow((adc_read(0)*2.5/255),(1.182033096926714)))));
 		//actual_value= (signed char)((uint8_t)(s1-s2));
 		
-		PORTB |= (1 << PINB0);
-		PORTB &= ~(1 << PINB1);
-		UART_transmitByte(adc_read(1));
-		_delay_ms(10);
-		PORTB |= (1 << PINB1);
-		PORTB &= ~(1 << PINB0);
-		UART_transmitByte(adc_read(0));
-		_delay_ms(10); 
+		// notera att PINB4 inte funkar.enbart jordning.
+		//sensor_right_front();
+		//sensor_right_back();
 		
-		//uint8_t Low =  I2C_receive(0x2C);
-		//uint8_t High = I2C_receive(0x2D);
+		sensor_distance();
+		//terminal_view(left_distance,average_distance);
+	
 		
-		//terminal_view(1,0);
 		
 	}
 }
@@ -70,15 +87,15 @@ void SendString(char mydata[20])
 	}
 }
 
-void terminal_view (int8_t adc_channel1, uint8_t adc_channel2)
+void terminal_view (uint8_t adc_channel1, uint8_t adc_channel2)
 {
-	adc_channel1&=0b00000111;
-	adc_channel2&=0b00000111;
+	//adc_channel1&=0b00000111;
+	//adc_channel2&=0b00000111;
 	char mychar1[5];
 	char mychar2[5];
 
 	SendString("ADC_value1: ");
-	sprintf(mychar1, "%04d", adc_read(adc_channel1));
+	sprintf(mychar1, "%04d", adc_channel1);
 	UART_transmitByte(mychar1[1]);
 	UART_transmitByte(mychar1[2]);
 	UART_transmitByte(mychar1[3]);
@@ -86,7 +103,7 @@ void terminal_view (int8_t adc_channel1, uint8_t adc_channel2)
 	_delay_ms(50);
 	
 	SendString("   ADC_value2: ");
-	sprintf(mychar2, "%04d", adc_read(adc_channel2));
+	sprintf(mychar2, "%04d", adc_channel2);
 	UART_transmitByte(mychar2[1]);
 	UART_transmitByte(mychar2[2]);
 	UART_transmitByte(mychar2[3]);
@@ -96,4 +113,110 @@ void terminal_view (int8_t adc_channel1, uint8_t adc_channel2)
 	
 	
 	
+} 
+
+
+void sensor_right_front(){
+	PORTB |= (1 << PINB0);
+	PORTB &= 0b00000001;
+	UART_transmitByte(adc_read(1));
+	_delay_ms(10);
 }
+
+void sensor_right_back(){
+	PORTB |= (1 << PINB1);
+	PORTB &= 0b00000010;
+	UART_transmitByte(adc_read(0));
+	_delay_ms(10);
+}
+
+void sensor_front(){
+	
+	PORTB |= 0b00000011;
+	PORTB &= 0b00000011;
+	UART_transmitByte(ceil( ( adc_read(2)+adc_read(3) ) / 2));
+	_delay_ms(10);
+}
+
+
+void sensor_left(){
+	
+	PORTB |= (1 << PINB2);
+	PORTB &= 0b00000100;	
+	UART_transmitByte(adc_read(4));
+	_delay_ms(10);
+}
+
+void sensor_gyro(){
+	
+	PORTB |= 0b00000101;
+	PORTB &= 0b00000101;
+	UART_transmitByte(I2C_receive(0x2C)); // low
+	_delay_ms(10);
+	
+	PORTB |= 0b00000110;
+	PORTB &= 0b00000110;
+	UART_transmitByte(I2C_receive(0x2D)); // high
+	_delay_ms(10);
+}
+
+void sensor_distance(void){
+	left_distance=lenght_left();
+	right_distance= lenght_right();
+	average_distance = (left_distance + right_distance) / 2;
+	
+	if ( average_distance >= 30)
+	{
+		PORTB |= 0b00000111;
+		PORTB &= 0b00000111;
+	}
+	else
+	{
+		PORTB = 0b00000000;
+	}
+	_delay_ms(10);
+}  
+ 
+uint8_t lenght_left()
+{
+	PB_4 = PINB & 0b00010000;
+	
+	
+	// black = low, white =high
+	if ((PB_4 == 0b00010000) && !(latest_color1==0b00010000))
+	{
+		white_4++;
+		latest_color1 = PB_4;
+		
+	}
+	else if ((PB_4 == 0b00000000) && !(latest_color1 == 0b00000000))
+	{
+		black_4++;
+		latest_color1=PB_4;
+	}
+	
+	return black_4 + white_4;
+}
+
+
+uint8_t lenght_right()
+{
+	PB_5 = PINB & 0b00100000;
+	
+	
+	// black = low, white =high
+	if ((PB_5 == 0b00100000) && !(latest_color2==0b00100000))
+	{
+		white_5++;
+		latest_color2 = PB_5;
+		
+	}
+	else if ((PB_5 == 0b00000000) && !(latest_color2 == 0b00000000))
+	{
+		black_5++;
+		latest_color2=PB_5;
+	}
+	
+	return black_5 + white_5;
+}
+
