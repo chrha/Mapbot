46b2db62f2b1520373f6a713e2fa434bc148d9fa
Ilian Ayoub <iliay038@student.liu.se>
2017-12-02 12:09:13 +0100

    Update com_module.c, with the other sensor values

--- src/com_module.c
+++ src/com_module.c
@@ -13,8 +13,19 @@ volatile unsigned char data_received_sensor;
 signed char KP=6;
 signed char KI=0;
 signed char KD=3; 
-volatile unsigned char sensor1=0;
-volatile unsigned char sensor2=0;
+
+volatile unsigned char sensor_right_front=0;
+volatile unsigned char sensor_right_back=0;
+
+volatile unsigned char sensor_front=0;
+
+volatile unsigned char sensor_left=0;
+volatile unsigned char sensor_gyro_low=0;
+volatile unsigned char sensor_gyro_high=0;
+volatile unsigned char sensor_distance=0;
+
+
+
 volatile int i=0;
 signed char latest_error=0;
 signed char I=0;
@@ -27,11 +38,26 @@ signed char error;
 signed char strong1=30;
 signed char strong2=-20;
 signed char weak1=10;
-signed char weak2=-5; 
+signed char weak2=-5; // -10
 
 volatile uint8_t temp0;
-
 volatile uint8_t temp1;
+volatile uint8_t temp2;
+volatile uint8_t temp3;
+volatile uint8_t temp4;
+volatile uint8_t temp5;
+volatile uint8_t temp6;
+
+
+void rotate_right(void);
+void rotate_left(void);
+void pid_forward(void);
+void backward(void);
+void forward(void);
+
+
+
+
 
 void terminal_view (uint8_t adc_channel1,uint8_t adc_channel2, uint8_t actual);
 
@@ -46,16 +72,29 @@ int main(void)
 	UART_init();
 	UART_usb_init();
 	sei();
+	
+
 	while(1)
-	{
+	{	
+		temp1 = PINA & 0b00000111;
+		_delay_ms(10);
+		if (temp1 == 0b00000111){
+			sensor_distance=1; //ändra sen till att lagra vägg
+		}
 		
-		PORTB |= (1 << PINB4);
-		PORTB &= ~(1 << PINB2);
-		PORTB &= ~(1 << PINB3);
-		PORTB &= ~(1 << PINB5);
-		_delay_ms(2);
+	
+	
+		if(sensor_distance == 0){
+			forward();
+		}else{
+			PORTB &= 0b00000000;
+		}
+	
 		
-		PID();
+		
+		
+		
+
 		
 		
 	}
@@ -68,27 +107,40 @@ ISR(USART0_RX_vect){
 	
 }
 ISR(USART1_RX_vect){
-	data_received_sensor = UDR1;
 	
-	temp0 = PINA & 0b00000001;
-	temp1 = PINA & 0b00000010;
+	data_received_sensor = UDR1;
+	//UART_usb_transmitByte(data_received_sensor);
+	temp0 = PINA & 0b00000111;
+
+	
 	if(temp0 == 1){
-		sensor1= data_received_sensor;
-		
+		sensor_right_front= data_received_sensor;
+	
 	}
-	else if(temp1 == 2){
-		sensor2=data_received_sensor;
+	else if(temp0 == 2){
+		sensor_right_back=data_received_sensor;
+	}else if (temp0 == 3){
+		sensor_front=data_received_sensor;
+	}else if (temp0 == 4){
+		sensor_left= data_received_sensor;
+	}else if (temp0 == 5){
+		sensor_gyro_low= data_received_sensor;
+	}else if (temp0 == 6){
+		sensor_gyro_high= data_received_sensor;
 	}
+
+	
 	
 
 
 }
+
 void PID(){
 	
 
 		
 	
-	actual_value= sensor1- sensor2;
+	actual_value= sensor_right_front- sensor_right_back;
 	error= desired_value - actual_value;
 		
 	P= error;
@@ -101,19 +153,19 @@ void PID(){
 	
 	latest_error=error;
 	
-	if((sensor1 > 181) && (sensor2 < 181) ){
+	if((sensor_right_front > 181) && (sensor_right_back < 181) ){
 		
 		UART_transmitByte((weak2+PID_value)); // lugnt
 		
-	}else if( (sensor1 > 181) && ( sensor2 > 181)){
+	}else if( (sensor_right_front > 181) && ( sensor_right_back > 181)){
 		
 		UART_transmitByte((strong2+PID_value)); //stark
 		
-	}else if((sensor1 < 152) && (sensor2 > 152)){ // 152
+	}else if((sensor_right_front < 152) && (sensor_right_back > 152)){ // 152
 		
 		UART_transmitByte((weak1+PID_value));  //lugn
 		
-	}else if((sensor1 < 152) && (sensor2 < 152)){
+	}else if((sensor_right_front < 152) && (sensor_right_back < 152)){
 		
 		UART_transmitByte((strong1+ PID_value)); //stark
 		
@@ -122,8 +174,47 @@ void PID(){
 	}
 		
 	
-	
-	
+}
 
+void rotate_right(){
+	
+	PORTB |= (1 << PINB2);
+	PORTB &= 0b00000100;
+	_delay_ms(7750);
+	PORTB &= 0b00000000;
+	_delay_ms(10);
 	
 }
+
+void rotate_left(){
+	
+	PORTB |= (1 << PINB3);
+	PORTB &= 0b00001000;
+	_delay_ms(7750);
+	
+	PORTB &= 0b00000000;
+	_delay_ms(10);
+}
+
+void pid_forward(){
+	
+
+	PORTB |= 0b00001100;
+	PORTB &= 0b00001100;
+	PID();
+	_delay_ms(10);
+}
+
+void backward(){
+	
+	PORTB |= 0b00010000;
+	PORTB &= 0b00010000;
+	_delay_ms(10);
+}
+
+void forward(){
+	
+	PORTB |= 0b00010100;
+	PORTB &= 0b00010100;
+	_delay_ms(10);
+}  
